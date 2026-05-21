f432f0d433815eed72fc71a4f15890c04296664d
Ronan <ronanantoque0@gmail.com>
2026-05-21 10:38:23 +0800

    please gana na ba

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,9 +1,11 @@
-import { catchError, of } from 'rxjs';
+import { catchError, of, timeout } from 'rxjs';
 import { AccountService } from '@app/_services';
 
 export function appInitializer(accountService: AccountService) {
     return () => accountService.refreshToken()
         .pipe(
+            // timeout after 5 seconds to ensure app starts even if backend is slow
+            timeout(5000),
             // catch error to start app on success or failure
             catchError(() => of())
         );
